89f87a1fadc4f4b1c90167cdb9a3be66a6ba181c
Thomas <thomas.farineau@icloud.com>
2023-09-12 14:46:52 +0200

    add db with visit middleware logger

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const {Sequelize} = require("sequelize");
+const {Sequelize, DataTypes} = require("sequelize");
 
 const app = express();
 
@@ -21,17 +21,64 @@ const sequelize = new Sequelize({
 })
 
 sequelize.authenticate().then(() => {
-    console.log('Connection has been established successfully.');
+    sequelize.sync().then(() => {
+        console.log('Database synced');
+    });
 }).catch(error => {
     console.error('Unable to connect to the database:', error);
 });
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({extended: false}));
 app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, 'public')));
+
+const Visit = require('./models/visit')(sequelize, DataTypes);
+
+function recordVisit(req, res, next) {
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    const url = req.originalUrl;
+    const agent = req.get('User-Agent');
+
+    Visit.create({
+        ip: ip, date: new Date(), url: url, agent: agent
+    })
+        .then(() => {
+            next();
+        })
+        .catch(err => {
+            console.error("Error recording visit:", err);
+            next(); // Continue to the next middleware even if saving fails
+        });
+}
+
+app.use(recordVisit);
+
+app.get('/db/getVisit', (req, res) => {
+    Visit.findAll()
+        .then(visits => {
+            res.json(visits);
+        })
+        .catch(err => {
+            console.error("Error fetching visits:", err);
+            res.status(500).send('Internal server error');
+        });
+});
+
+// Route pour obtenir le nombre total de visites
+app.get('/db/countVisit', (req, res) => {
+    Visit.count()
+        .then(count => {
+            res.json({totalVisits: count});
+        })
+        .catch(err => {
+            console.error("Error counting visits:", err);
+            res.status(500).send('Internal server error');
+        });
+});
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
